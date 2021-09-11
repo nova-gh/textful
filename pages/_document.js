3cecaf775daf228db66ee2b3d7306f4c79b0a4ad
Nova Gh <nghimire.dev@gmail.com>
2021-09-10 21:58:03 -0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,11 @@ class MyDocument extends Document {
         <Head>
           {/* Fonts */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
           <link
             href="https://fonts.googleapis.com/css2?&family=Poppins:wght@400;500;600;700&display=swap"
             rel="stylesheet"
