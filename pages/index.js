7b90d093b295b2569c0bc43d84e512dd52c177ad
Nova Gh <nghimire.dev@gmail.com>
2021-09-10 21:58:14 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,13 @@ import MainWrapper from "../components/UI/MainWrapper";
 import Hero from "../components/UI/Hero";
 export default function Home() {
   return (
-    <div className="">
+    <div className="min-h-screen">
       <Head>
         <title>textFul</title>
+        <meta
+          name="description"
+          content="Create notes and access them via URL"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Hero />
