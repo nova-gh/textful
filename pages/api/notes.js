5753136966db9b34720dd714ffb3e815095d0c01
Nova Gh <nghimire.dev@gmail.com>
2021-09-11 00:02:18 -0700

    Update notes.js

--- pages/api/notes.js
+++ pages/api/notes.js
@@ -1,13 +1,26 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// db connection
+import { dbConnect } from "../../lib/dbConnect";
+import { Note } from "../../model/Note";
+import * as randomstring from "randomstring";
+export default async (req, res) => {
+  // call db connection
+  await dbConnect();
 
-export default function (req, res) {
   // only listen to POST reqs
-  if (req.method === 'POST') {
-    res.status(200).json({ name: "John Doe" });
+  if (req.method === "POST") {
+    // we use randomstring libary to genrate slugs
+    const slug = randomstring.generate({
+      length: 6,
+      charset: "alphabetic",
+    });
+    // create Note from model from req/res
+    const note = await Note.create({
+      note: req.body.note,
+      slug,
+      // we use randomstring libary to genrate slugs
+    });
+    res.status(200).json(note);
   } else {
-    throw new Error {
-      "HTTP method not supported in this endpoint!"
-    }
+    throw new Error("HTTP method not supported in this endpoint!");
   }
-  
-}
+};
