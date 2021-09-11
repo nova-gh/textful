342d67f0682fcaed4b538d0c46435dbcab52ac7f
Nova Gh <nghimire.dev@gmail.com>
2021-09-10 21:58:11 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        vh60: "60vh",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
