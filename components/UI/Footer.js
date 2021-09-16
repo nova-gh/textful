b4506d622e6ce38a4e50e508921f484a6ad43a8e
Nova Gh <nghimire.dev@gmail.com>
2021-09-16 13:51:00 -0700

    Update Footer.js

--- components/UI/Footer.js
+++ components/UI/Footer.js
@@ -1,17 +1,26 @@
+import { SiGithub, SiLinkedin } from "react-icons/si";
 const Footer = () => {
   return (
     <>
-      <footer className="flex items-center justify-center w-full h-24 border-t">
+      <footer className="flex items-center justify-center w-full h-24 space-x-3 border-t">
         <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          className="flex items-center justify-center "
+          href="https://github.com/nova-gh/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
+          <SiGithub size="26" className="hover:text-blue-300" />
+        </a>
+        <a
+          className="flex items-center justify-center"
+          href="https://www.linkedin.com/in/nawadip-ghimire/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <SiLinkedin size="26" className="hover:text-blue-300" />
         </a>
       </footer>
+      <h2>test</h2>
     </>
   );
 };
