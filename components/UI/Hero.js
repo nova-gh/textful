454a7bd7200ed309e4ef1796a130b93f278b3df8
Nova Gh <nghimire.dev@gmail.com>
2021-09-16 13:50:54 -0700

    Update Hero.js

--- components/UI/Hero.js
+++ components/UI/Hero.js
@@ -23,14 +23,10 @@ const Hero = () => {
           </a>
           <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
             <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-              <span className="relative inline-block">
-                <span className="relative">The</span>
-              </span>{" "}
-              quick, brown fox jumps over a lazy dog
+              Create quick mini Notes on the go and access them with a url!
             </h2>
             <p className="text-base text-gray-700 md:text-lg">
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque rem aperiam, eaque ipsa quae.
+              Powered by Next.js, MongoDb and Tailwind.
             </p>
           </div>
           <div>
